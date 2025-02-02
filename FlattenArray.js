0d5ec0fbddb9754cfc5e35f244be1b074122839b
MuhammadSarimWaseem <sarimwaseem84@gmail.com>
2025-02-02 19:56:53 +0500

    updated

--- FlattenArray.js
+++ FlattenArray.js
@@ -1,3 +1,14 @@
-const array = [1, 2, [3, 4, [5, 6, 3, 2, [5, 6, 7]]]]
-const flatArray=array.flat(Infinity)
-console.log(flatArray);
+function flat(arr, n) {
+    let result = arr.flat(n)
+    return result
+
+};
+
+let arr1 = flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 0)
+let arr2 = flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 1)
+let arr3 = flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 2)
+
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
